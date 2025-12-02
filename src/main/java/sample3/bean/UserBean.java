26a86f2f42e98366e822200cf8fedf6d960752dd
yesboo <nobut125au@gmail.com>
2025-12-03 06:19:31 +0900

    UPDATE User status is succeed

--- src/main/java/sample3/bean/UserBean.java
+++ src/main/java/sample3/bean/UserBean.java
@@ -2,7 +2,10 @@ package sample3.bean;
 
 import org.postgresql.util.PSQLException;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import sample3.dao.UserDao;
 import sample3.model.User;
@@ -11,9 +14,29 @@ import sample3.model.User;
 @RequestScoped
 public class UserBean {
     private User user = new User();
-   
+
+    @PostConstruct
+    public void init(){
+        //リクエストパラメータからuserIDを取得
+        FacesContext  context = FacesContext.getCurrentInstance();
+        String strUID = context.getExternalContext().getRequestParameterMap().get("userId");
+
+        if (strUID != null){
+            try {
+                Integer intUID = Integer.parseInt(strUID);
+                user = UserDao.findById(intUID);
+            } 
+            catch (NumberFormatException  e) {
+            }
+        }
+    }
+    
     public User getUser() {
         return user;
+    }
+
+    public void setUser(User user){
+        this.user = user;
     }
 
     public String createUser() {
@@ -35,5 +58,20 @@ public class UserBean {
     private String hash(String plain) {
         // ハッシュ化処理（簡易例）
         return Integer.toHexString(plain.hashCode());
+    }
+
+    public String updateUser(){
+        try {
+            if (new UserDao().updateUser(user) == 1){
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "ユーザー情報を更新しました。", null));
+            }else{
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "更新に失敗しました。", null));
+            }
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "更新に失敗しました。", null));
+            return null;   
+        }
     }
 }
--- src/main/java/sample3/dao/UserDao.java
+++ src/main/java/sample3/dao/UserDao.java
@@ -7,8 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.postgresql.util.PSQLException;
-
 import sample3.model.User;
 
 public class UserDao {
@@ -38,6 +36,30 @@ public class UserDao {
         return list;
     }
 
+    public static User findById(Integer id){
+        String sql = "SELECT id,password,acname,role,name,email FROM users WHERE id = ?";
+
+        try {
+            Connection con = ConDao.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            User u = new User();
+            while(rs.next()){
+                u.setId(rs.getInt("id"));
+                u.setPassword(rs.getString("password"));
+                u.setAcname(rs.getString("acname"));
+                u.setRole(rs.getInt("role"));
+                u.setName(rs.getString("name"));
+                u.setEmail(rs.getString("email"));
+            }
+            return u;
+        } catch (Exception e) {
+            throw new RuntimeException("ユーザー一覧取得に失敗", e);
+        }
+    }
+
     public void insertUser(User user) throws Exception {
         String sql = "INSERT INTO users (acname, password, role, name, email) VALUES (?, ?, ?, ?, ?)";
 
@@ -50,19 +72,35 @@ public class UserDao {
             ps.setString(4, user.getName());
             ps.setString(5, user.getEmail());
 
-            ps.executeUpdate();
-        } catch (PSQLException e) {
-            System.err.println("PostgreSQL例外発生:");
-            System.err.println("メッセージ: " + e.getMessage());
-            System.err.println("SQLState: " + e.getSQLState());
-            System.err.println("サーバエラー: " + e.getServerErrorMessage());
-            e.printStackTrace();
+            ps.executeUpdate();;
         } catch (SQLException e) {
             System.err.println("SQL例外発生:");
             System.err.println("メッセージ: " + e.getMessage());
             System.err.println("SQLState: " + e.getSQLState());
             e.printStackTrace();
         }
-
     }
+
+    public int updateUser(User user) throws Exception{
+        String sql = "UPDATE users SET acname=?, role=?, name=?, email=? WHERE id = ?";
+
+        try (Connection conn = ConDao.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, user.getAcname());
+            ps.setInt(2, user.getRole());
+            ps.setString(3, user.getName());
+            ps.setString(4, user.getEmail());
+            ps.setInt(5, user.getId());
+
+            return ps.executeUpdate();  //更新されれば、１件以上を返す。
+        } catch (SQLException e) {
+            System.err.println("SQL例外発生:");
+            System.err.println("メッセージ: " + e.getMessage());
+            System.err.println("SQLState: " + e.getSQLState());
+            e.printStackTrace();
+            return 0;   //更新件数０件とする。
+        }
+    }
+
 }
